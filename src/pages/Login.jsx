60ed80fa9c76ded6c695fbc38f08bbe1f32b9745
vishneelu <sharmadollyneelu1999@gmail.com>
2025-10-03 00:11:50 +0530

    login signup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,62 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { auth } from "../firebase";
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState({});
+
+  const validate = () => {
+    let tempErrors = {};
+    if(!email){
+      tempErrors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(email)){
+      tempErrors.email = "Email is not valid";
+    }
+    if(!password){
+      tempErrors.password = "Password is required";
+    }else if(password.length<6){
+      tempErrors.password = "Password must be at least 6 characters";
+    }
+    setError(tempErrors);
+
+    return Object.keys(tempErrors).length= 0;
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+   if(validate()){
+      console.log("Email", email, "Password", password)
+   }
+   
+  }
   return (
-    <div>Login</div>
+    <div className='flex items-center justify-center min-h-screen bg-gray-100'>
+      <div className='w-full max-w-md bg-white rounded-2xl shadow-lg p-8'>
+        <h2 className='text-2xl font-bolc text-center text-gray-800 mb-6'>Login</h2>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div className='mb-4'>
+            <label className='block text-gray-700 font-medium mb-1'>Email</label>
+            <input type='email' value={email} onChange={(e)=> setEmail(e.target.value)} placeholder='Enter your email'  className={`w-full px-4 py-2 border rounded-xl focus:outline-none 
+                ${error.email ? "border-red-500 focus:ring-2 focus:ring-red-400" : "border-gray-300 focus:ring-2 focus:ring-blue-500"}`}
+           />
+            {error.email && (
+              <p className="text-red-500 text-sm mt-1">{error.email}</p>
+            )}
+          </div>
+          <div className='mb-8'>
+            <label className='block text-gray-700 font-medium mb-1'>Password</label>
+            <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Enter your password' className={`w-full px-4 py-2 border rounded-xl focus:outline-none 
+                ${error.password ? "border-red-500 focus:ring-2 focus:ring-red-400" : "border-gray-300 focus:ring-2 focus:ring-blue-500"}`}
+           />
+            {error.password && (
+              <p className="text-red-500 text-sm mt-1">{error.password}</p>
+            )}
+          </div>
+          <button type='submit' className='w-full mb-4 bg-blue-600 text-white py-2 rounded-xl hover:bg-blue-700 transition duration-200'>Login</button>
+          <p className='text-center'>Don’t have an account?{" "}
+            <a href='/signup' className='text-blue-600 hover:underline'>Register</a>
+          </p>
+        </form>
+      </div>
+    </div>
   )
 }
